4c05712ccf3c59d5c99c5eab6eb21e28762e1f1c
Praateek Gunday <mailtopraateek@gmail.com>
2024-09-18 22:40:03 -0400

    edited to hello world

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: []
 })
-export class AppComponent {
-  title = 'my-angular-app';
-}
+export class AppComponent {}
